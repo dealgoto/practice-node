d15618842449dd0bc593722f51e1b94e0f2c0910
Zheleznyak Maxim <134913522+dealgoto@users.noreply.github.com>
2024-06-21 16:08:21 +0300

    Delete 2 laba directory

--- 2 laba/api/start.js
+++
@@ -1,6 +0,0 @@
-//app.js
-const app = require('./app.js');
-
-app.listen(3000, () => {
-    console.log('Server start at localhost:3000');
-});
--- 2 laba/proxy/server/start.js
+++
@@ -1,7 +0,0 @@
-
-const app = require('../router/app.js');
-var conf = require('../service/config.js');
-
-app.listen(conf.get('port'), () => {
-    console.log('Server start at localhost:3001');
-});
